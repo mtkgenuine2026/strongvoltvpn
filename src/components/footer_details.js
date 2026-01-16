c435a42003f1178ba2ed464639acff92fe3a01f1
mtkgenuine2026 <mtkgenuine@gmail.com>
2026-01-16 15:37:28 +0800

    Update footer_details.js

--- src/components/footer_details.js
+++ src/components/footer_details.js
@@ -4,7 +4,7 @@ const FooterDetails = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h2 className="footer-title">MEDIATEK VPN</h2>
+        <h2 className="footer-title">STRONG VOLT VPN</h2>
         <p className="footer-copy">&copy; 2024 Dexter Eskalarte. All rights reserved.</p>
       </div>
     </footer>
