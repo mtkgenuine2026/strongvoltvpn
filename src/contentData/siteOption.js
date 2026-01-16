6c82217d799f184c88d6846bd8b04e6bf5b0c04f
mtkgenuine2026 <mtkgenuine@gmail.com>
2026-01-16 15:53:42 +0800

    Update siteOption.js

--- src/contentData/siteOption.js
+++ src/contentData/siteOption.js
@@ -3,7 +3,7 @@ const siteOptionData = [
       id: 1,
       siteName: "Strong Volt Vpn Apps",
       image:
-        "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+        "https://i.ibb.co/67rCfs8p/favicon.png",
       description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
       siteLink: "https://mtk-genuine.com/"
     }
