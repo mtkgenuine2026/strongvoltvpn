12efca0c36a4e68ff6f96dbb9871f1ad311f7294
mtkgenuine2026 <mtkgenuine@gmail.com>
2026-03-06 09:24:32 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,23 +1,24 @@
 const productData = [
-    {
-      id: 1,
-      name: "Strong Volt Vpn",
-      image:"https://i.ibb.co/67rCfs8p/favicon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-      rating: 5,
-      timeLeft: 27,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltvpn.apk"
-    },
-   {
-      id: 2,
-      name: "Strong Volt XRAY",
-      image:"https://i.ibb.co/67rCfs8p/favicon.png",
-      productType: "openBackHeadphones",
-      description: ["XRAY"],
-      rating: 5,
-      timeLeft: 27,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltxray-release.apk?inline=false"
-    }
-  ];
-  export default productData;
+  {
+    id: 1,
+    name: "Strong Volt Vpn",
+    image: "https://i.ibb.co/67rCfs8p/favicon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+    rating: 5,
+    timeLeft: 27,
+    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltvpn.apk"
+  },
+  {
+   id: 2,
+    name: "Strong Volt Xray",
+    image: "https://i.ibb.co/67rCfs8p/favicon.png",
+    productType: "openBackHeadphones",
+    description: ["XRAY"],
+    rating: 5,
+    timeLeft: 24,
+    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltxray-release.apk"
+  }
+];
+
+export default productData;
