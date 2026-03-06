dd72c393be400c6eba0d929a35c2d5e43f123c73
mtkgenuine2026 <mtkgenuine@gmail.com>
2026-03-06 09:17:18 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -9,16 +9,15 @@ const productData = [
       timeLeft: 27,
       appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltvpn.apk"
     }
-   // {
-   //   id: 3,
-   //   name: "STARLINK VPN LOGIN",
-   //   image:
-   //     "https://i.ibb.co/NFWPLbQ/app-main-logo.png",
-   //   productType: "openBackHeadphones",
-   //   description: ["OVPN", "UDP", "V2RAY"],
-   //   rating: 5,
-   //   timeLeft: 27,
-   //   appLink: "https://play.google.com/store/apps/details?id=app.fixbuild.StarlinkVPNv2"
-   // }
+   {
+      id: 1,
+      name: "Strong Volt XRAY",
+      image:"https://i.ibb.co/67rCfs8p/favicon.png",
+      productType: "openBackHeadphones",
+      description: ["XRAY"],
+      rating: 5,
+      timeLeft: 27,
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltxray-release.apk?inline=false"
+    }
   ];
   export default productData;
