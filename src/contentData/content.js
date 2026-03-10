7710843ad459ef68f1408d01e8884fb51c013959
mtkgenuine2026 <mtkgenuine@gmail.com>
2026-03-10 16:17:43 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -8,16 +8,16 @@ const productData = [
     rating: 5,
     timeLeft: 27,
     appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltvpn.apk"
-  },
-  {
-   id: 2,
-    name: "Strong Volt Xray",
-    image: "https://i.ibb.co/67rCfs8p/favicon.png",
-    productType: "openBackHeadphones",
-    description: ["XRAY"],
-    rating: 5,
-    timeLeft: 24,
-    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltxray-release.apk"
+ // },
+ // {
+ //  id: 2,
+  //  name: "Strong Volt Xray",
+   // image: "https://i.ibb.co/67rCfs8p/favicon.png",
+  //  productType: "openBackHeadphones",
+  //  description: ["XRAY"],
+  //  rating: 5,
+ //   timeLeft: 24,
+ //   appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/strongvoltxray-release.apk"
   }
 ];
 
